9cdce46ccbcce437022e52af0aa739db507038ee
Aarjan <aarjan.pat@gmail.com>
2023-03-01 18:11:07 +0545

    updated the logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css';
+import logo from './logo.png'
 
 function Navbar() {
 
@@ -21,9 +22,7 @@ function Navbar() {
   return (
     <div className={`nav  ${show && 'nav--black'}`}>
         <div className='nav--contents'>
-            <h1 className='nav--logo'>
-                OMOVIES
-            </h1>
+            <img className='nav--logo' src={logo}/>
             <img className='nav--avatar' src="http://cdn.onlinewebfonts.com/svg/img_162386.png" alt=""/>
         </div>
     </div>
