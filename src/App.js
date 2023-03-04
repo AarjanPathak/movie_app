bf253bc2e7d985fca8b46dd14d1c8bb81dda58aa
Aarjan <aarjan.pat@gmail.com>
2023-03-04 12:01:15 +0545

    login component updated

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Homescreen from "./components/Homescreen/Homescreen"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from './components/LoginScreen/LoginScreen';
+import { useEffect } from 'react';
+import { auth } from './components/firebase';
 
 function App() {
+
+  const user = null;
+
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(userAuth=>{
+      if(userAuth){
+        //logged in
+      }else{
+        //Logged out
+      }
+    });
+    return unsubscribe;
+  } ,[])
+
   return (
     <div className="App">
       <Router>
+        {!user ? (
+          <LoginScreen />
+        ): (
         <Routes>
-          <Route path="/" element={<LoginScreen/>} />
+          <Route path="/" element={<Homescreen/>} />
         </Routes>
+        )}
       </Router>
     </div>
   );
