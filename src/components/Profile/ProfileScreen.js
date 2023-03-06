23787f3c67a653d66fa45e40ad6f812bf5846464
Aarjan <aarjan.pat@gmail.com>
2023-03-06 10:49:56 +0545

    added the profile screen component

---
+++ src/components/Profile/ProfileScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileScreen.css'
+
+function ProfileScreen() {
+  return (
+    <div>
+      <h1>This is a profile</h1>
+    </div>
+  )
+}
+
+export default ProfileScreen
