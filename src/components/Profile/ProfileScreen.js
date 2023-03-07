00071079a906e1f5a84e58f858998ffd724cbe75
Aarjan <aarjan.pat@gmail.com>
2023-03-07 15:59:40 +0545

    updated the login and logout functionality

--- src/components/Profile/ProfileScreen.js
+++ src/components/Profile/ProfileScreen.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../features/userSlice'
+import { auth } from '../firebase'
 import Navbar from '../Navigation Bar/Navbar'
 import './ProfileScreen.css'
 
 function ProfileScreen() {
+
+  const user = useSelector(selectUser)
+
   return (
     <div className='profile'>
       <Navbar />
@@ -11,11 +17,11 @@ function ProfileScreen() {
         <div className='profile--info'>
           <img className="profile--logo" src="http://cdn.onlinewebfonts.com/svg/img_162386.png" alt=""/>
           <div className='profile--plans'>
-            <h2 className='email'>Email: abc@gmail.com</h2>
+            <h2 className='email'>Email: {user.email}</h2>
             <h2 className='plans'>Current Plan: none</h2>
           </div>
         </div>
-        <button className='signOut--btn'>Sign out</button>
+        <button onClick={()=> auth.signOut()}   className='signOut--btn'>Sign out</button>
       </div>
     </div>
   )
